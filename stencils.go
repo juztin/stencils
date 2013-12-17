29c79985eeaf75bafefc9839c6292c2dfcd849b9
Justin Wilson <justinwilson1@gmail.com>
2013-12-17 13:40:32 -0700

    Writing status code, implemented custom error

--- stencils.go
+++ stencils.go
@@ -17,6 +17,10 @@ type Stencils struct {
 	FiveOh *Stencil
 }
 
+func (e Error) Error() string {
+	return e.error.Error()
+}
+
 type StencilFn func(name string) *Stencil
 
 func New(fn StencilFn) *Stencils {
@@ -24,6 +28,20 @@ func New(fn StencilFn) *Stencils {
 	c.col = make(map[string]*Stencil)
 	c.create = fn
 	return c
+}
+
+func etchErr(status int, wr io.Writer, r *http.Request, err error, s *Stencil) *Error {
+	Log(err)
+
+	if r, ok := wr.(http.ResponseWriter); ok {
+		r.WriteHeader(status)
+	}
+
+	if s != nil {
+		// Todo, concat errors
+		s.Etch(wr, r, data)
+	}
+	return NewError(status, err)
 }
 
 func (c *Stencils) New(name string) *Stencil {
@@ -37,16 +55,17 @@ func (c *Stencils) Add(s *Stencil) *Stencils {
 	return c
 }
 
-func (c *Stencils) Etch(n string, wr io.Writer, r *http.Request, data interface{}) error {
+func (c *Stencils) Etch(n string, wr io.Writer, r *http.Request, data interface{}) *Error {
 	if s, ok := c.Name(n); ok {
 		var buf bytes.Buffer
 		if err := s.Etch(&buf, r, data); err != nil {
-			return err
+			return etchErr(500, wr, r, err, c.FiveOh)
+		} else if _, err = buf.WriteTo(wr); err != nil {
+			return etchErr(500, wr, r, err, c.FiveOh)
 		}
-		_, err := buf.WriteTo(wr)
-		return err
+		return nil
 	}
-	return fmtErr("Stencil does not exist: %s", n)
+	return etchErr(404, wr, r, fmtErr("Stencil does not exist: %s", n), c.FourOh)
 }
 
 func (c *Stencils) Name(n string) (*Stencil, bool) {
